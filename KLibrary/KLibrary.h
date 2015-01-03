48c5064e45a1c515914fd43dd71677045c153905
Koji Okazaki <kj.okzk@gmail.com>
2015-01-03 14:58:18 +0900

    Initial Commit

---
+++ KLibrary/KLibrary.h
@@ -0,0 +1,19 @@
+//
+//  KLibrary.h
+//  KLibrary
+//
+//  Created by KO on 2015/01/03.
+//  Copyright (c) 2015年 KO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KLibrary.
+FOUNDATION_EXPORT double KLibraryVersionNumber;
+
+//! Project version string for KLibrary.
+FOUNDATION_EXPORT const unsigned char KLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KLibrary/PublicHeader.h>
+
+
